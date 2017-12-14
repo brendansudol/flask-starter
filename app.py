296fca93fca1d2f34fdcaca478c1eb9dae3aafe5
Brendan Sudol <brendan.sudol@gsa.gov>
2017-12-14 14:52:28 -0500

    basic auth

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import logging
 
 from flask import Flask, render_template
+from flask_basicauth import BasicAuth
 
 
 app = Flask(__name__)
@@ -10,8 +11,16 @@ stream_handler = logging.StreamHandler()
 app.logger.addHandler(stream_handler)
 app.logger.setLevel(logging.INFO)
 
+app.config.update({
+    'BASIC_AUTH_USERNAME': 'foo',
+    'BASIC_AUTH_PASSWORD': 'bar',
+})
+
+auth = BasicAuth(app)
+
 
 @app.route('/')
+@auth.required
 def home():
     return render_template('index.html')
 
